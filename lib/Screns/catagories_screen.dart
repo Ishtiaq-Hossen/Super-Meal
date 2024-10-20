8c27e439ac41168d8fa0adf440d5e07168c7bcd3
Ishtiaq-Hossen <ishtiaqhossen321@gmail.com>
2024-10-20 16:40:28 +0600

    Animation applied in meal detail,meal item and catagoryScreen.

--- lib/Screns/catagories_screen.dart
+++ lib/Screns/catagories_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 import 'package:super_meal_v2/data/dummy_data.dart';
 import 'package:super_meal_v2/models/category.dart';
@@ -6,7 +7,7 @@ import 'package:super_meal_v2/widgets/category_grid_item.dart';
 import '../models/meal.dart';
 import 'meals_screen.dart';
 
-class CatagoriesScreen extends StatelessWidget {
+class CatagoriesScreen extends StatefulWidget {
   final List<Meal> availableMeals;
 
   const CatagoriesScreen({
@@ -15,9 +16,14 @@ class CatagoriesScreen extends StatelessWidget {
     required this.availableMeals,
   });
 
+  @override
+  State<CatagoriesScreen> createState() => _CatagoriesScreenState();
+}
+
+class _CatagoriesScreenState extends State<CatagoriesScreen> with SingleTickerProviderStateMixin{
   void _onSelectCatagory(BuildContext context, Category category) {
     //filtering by category
-    final filteredMeals = availableMeals
+    final filteredMeals = widget.availableMeals
         .where((meal) => meal.categories.contains(category
             .id)) //ekta ekta meal check korbe .selected meal er catagory meal e true nahoi false
         .toList();
@@ -33,28 +39,55 @@ class CatagoriesScreen extends StatelessWidget {
                 ))
     );
   }
-
+  late AnimationController _animationController ;
+  @override
+  void initState() {
+    _animationController=AnimationController(
+        vsync:this,
+      duration: Duration(milliseconds: 300),
+      lowerBound: 0,//animation suru hobe kotha theke
+      upperBound: 1
+    );
+    _animationController.forward();
+    super.initState();
+  }
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: EdgeInsets.all(10),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20),
-      children: [
-        // availableCategory.map((category)=>
-        // CategoryGridItem(category: category)
-        // ).toList()//eta use korle children er [] utay dite hobe
-        for (final category in availableCategories)
-          CategoryGridItem(
-            category: category,
-            onSelectCategory: () {
-              _onSelectCatagory(context, category);
-            },
-          )
-      ],
+    return AnimatedBuilder(
+      animation: _animationController,
+      child:GridView(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: [
+          // availableCategory.map((category)=>
+          // CategoryGridItem(category: category)
+          // ).toList()//eta use korle children er [] utay dite hobe
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _onSelectCatagory(context, category);
+              },
+            )
+        ],
+      ),
+      builder: (context,child) => SlideTransition(
+        position: Tween(
+          begin: Offset(0,0.3),
+          end: Offset(0,0),
+        ).animate(CurvedAnimation(
+            parent: _animationController, curve: Curves.easeInOut)),
+          child: child,
+      ),
+
     );
   }
 }
--- lib/Screns/meal_detail_screen.dart
+++ lib/Screns/meal_detail_screen.dart
@@ -32,18 +32,34 @@ class MealDetailScreen extends ConsumerWidget {
                 ))
               );
             },
-            icon: isFavourite? Icon(Icons.favorite): Icon(Icons.favorite_border),
+            icon: AnimatedSwitcher(
+              duration: Duration(milliseconds: 300),
+              transitionBuilder: (child,animation){
+                return RotationTransition(turns: Tween<double>(
+                  begin: 0,
+                  end: 1,
+                ).animate(animation),
+                child: child);
+              },
+              child: Icon(
+                isFavourite? Icons.favorite: Icons.favorite_border,
+                key: ValueKey(isFavourite),
+              ),
+            ),
           )
         ],
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-          Image.network(
-            meal.imageUrl,
-            height: 300,
-            width: double.infinity,
-            fit: BoxFit.cover,
+          Hero(
+            tag: meal.id,
+            child: Image.network(
+              meal.imageUrl,
+              height: 300,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
           ),
             SizedBox(height: 16),
             Text(
